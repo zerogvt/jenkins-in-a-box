792a4df8e88cfc40a7507215cb079d82f1342991
vas <white@fluffy.bun>
2018-03-23 17:26:08 +0000

    ...---...

--- build-and-run-me.sh
+++ build-and-run-me.sh
@@ -6,5 +6,7 @@ set -e
 source jenkins.env; 
 rm -rf docker-compose.yml; envsubst < "docker-compose-template.yml" > "docker-compose.yml";
 rm -rf jenkins-ansible.yml; envsubst < "jenkins-ansible-template.yml" > "jenkins-ansible.yml";
+ls -la
+sleep 10
 docker-compose build
 docker-compose up
