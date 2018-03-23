840226f0adb846717d8e90da6c1a7adcbaea4500
vas <white@fluffy.bun>
2018-03-23 17:20:42 +0000

    blinking potatoes

--- build-and-run-me.sh
+++ build-and-run-me.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+
+#fail fast
+set -e
+
 source jenkins.env; 
 rm -rf docker-compose.yml; envsubst < "docker-compose-template.yml" > "docker-compose.yml";
 rm -rf jenkins-ansible.yml; envsubst < "jenkins-ansible-template.yml" > "jenkins-ansible.yml";
