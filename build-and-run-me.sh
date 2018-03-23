6a99f6964e6959f00e7c16a471370fe2f4542aed
vas <white@fluffy.bun>
2018-03-23 16:27:16 +0000

    rm temp (but then again aren't we all temps?)

--- build-and-run-me.sh
+++ build-and-run-me.sh
@@ -2,3 +2,5 @@
 source jenkins.env; 
 rm -rf docker-compose.yml; envsubst < "docker-compose-template.yml" > "docker-compose.yml";
 rm -rf jenkins-ansible.yml; envsubst < "jenkins-ansible-template.yml" > "jenkins-ansible.yml";
+docker-compose build
+docker-compose up
