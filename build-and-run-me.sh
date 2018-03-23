ac821275b2f6d745826e56f28af8e93e2595b0af
vas <white@fluffy.bun>
2018-03-23 18:06:52 +0000

    how is a bad joke crafted? pun by pun

--- build-and-run-me.sh
+++ build-and-run-me.sh
@@ -6,7 +6,6 @@ set -e
 source jenkins.env; 
 rm -rf docker-compose.yml; envsubst < "docker-compose-template.yml" > "docker-compose.yml";
 rm -rf jenkins-ansible.yml; envsubst < "jenkins-ansible-template.yml" > "jenkins-ansible.yml";
-ls -la
-sleep 10
+
 docker-compose build
 docker-compose up
