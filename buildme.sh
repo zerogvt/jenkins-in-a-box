76b04874db5f306c0cbd4613f6a5c79792079cdb
vas <white@fluffy.bun>
2018-03-23 15:39:31 +0000

    then there was a big loud sound

--- buildme.sh
+++ buildme.sh
@@ -1,1 +1,4 @@
-source jenkins.env; rm -rf docker-compose.yml; envsubst < "docker-compose-template.yml" > "docker-compose.yml";
+#!/bin/bash
+source jenkins.env; 
+rm -rf docker-compose.yml; envsubst < "docker-compose-template.yml" > "docker-compose.yml";
+rm -rf jenkins-ansible.yml; envsubst < "jenkins-ansible-template.yml" > "jenkins-ansible.yml";
