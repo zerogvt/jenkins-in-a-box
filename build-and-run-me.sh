a1e6abf68560a4944e4d3ad12c41dfb7df8f8e90
zerogvt <zerogvt@github.com>
2018-03-24 11:33:46 +0000

    pre-provision slaves

--- build-and-run-me.sh
+++ build-and-run-me.sh
@@ -3,9 +3,19 @@
 #fail fast
 set -e
 
+# read our env vars
 source jenkins.env; 
+
+# fix our docker and ansible templates according to our env (ENV_SUBTITUTION)
 rm -rf docker-compose.yml; envsubst < "docker-compose-template.yml" > "docker-compose.yml";
 rm -rf jenkins-ansible.yml; envsubst < "jenkins-ansible-template.yml" > "jenkins-ansible.yml";
 
+# create jenkins slaves configs
+rm -rf slave_1.xml; export SLAVE_NAME=${SLAVE_1_NAME}; envsubst < "slave-template.xml" > "slave_1.xml";
+rm -rf slave_2.xml; export SLAVE_NAME=${SLAVE_2_NAME}; envsubst < "slave-template.xml" > "slave_2.xml";
+
+
 docker-compose build
 docker-compose up
+
+
